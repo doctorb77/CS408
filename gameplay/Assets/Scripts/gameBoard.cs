e6015aff6e68edfbee7eb2f2faac7b2a9f7417a9
Benjamin Loisch <ben.loisch@gmail.com>
2019-02-06 23:03:15 -0500

    Grid and Click
    Added a grid to the board and the ability to selected a unit then move it to another spot on the board

--- gameplay/Assets/Scripts/gameBoard.cs
+++ gameplay/Assets/Scripts/gameBoard.cs
@@ -7,20 +7,39 @@ public class gameBoard : MonoBehaviour
     public static gameBoard Instance { set; get; }
 
     //public terrainTiles[,] terrain = new terrainTiles[10, 10];
+    public string[,] boardTileInstanceTypes = new string[10, 10];
+    public GameObject[,] boardTileInstances = new GameObject[10, 10];
     public Vector3 pz = new Vector3(-5.0f, 0.0f, 0.0f);
 
     public GameObject terrain1Prefab;
+    public GameObject melee1;
+
+    public GameObject selectedUnit;
+
     public GameObject test;
 
-    private Vector2 mouseOver;
+    private Vector2 lastClicked;
 
 
     private void Start()
     {
         Instance = this;
 
-        test = Instantiate(terrain1Prefab) as GameObject;
-        
+        selectedUnit = null;
+
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)
+                boardTileInstances[i, j] = null;
+
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)
+                boardTileInstanceTypes[i, j] = "empty";
+
+        test = Instantiate(melee1) as GameObject;
+        test.transform.position = new Vector3(0, 0, -1);
+        boardTileInstances[0, 0] = test;
+        boardTileInstanceTypes[0, 0] = "melee1";
+
     }
 
     private void Update()
@@ -30,13 +49,36 @@ public class gameBoard : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("BoardLayer")))
             {
-                mouseOver.x = (hit.point.x);
-                mouseOver.y = (hit.point.y);
+                int currMouseX = (int)Mathf.Round((hit.point.x));
+                int currMouseY = (int)Mathf.Round((hit.point.y));
 
-                test.transform.position = new Vector3(mouseOver.x, mouseOver.y, 0);
+                Debug.Log(lastClicked);
 
+                if (boardTileInstanceTypes[currMouseX, currMouseY] == "empty")
+                {
+                    if (selectedUnit == null)
+                        return;
+                    else
+                    {
+                        selectedUnit.transform.position = new Vector3(currMouseX, currMouseY, -1);
+                        boardTileInstanceTypes[(int)lastClicked.x, (int)lastClicked.y] = "empty";
+                        boardTileInstances[(int)lastClicked.x, (int)lastClicked.y] = null;
+                        boardTileInstanceTypes[currMouseX, currMouseY] = "melee1";
+                        boardTileInstances[currMouseX, currMouseY] = selectedUnit;
+                        selectedUnit = null;
+                        lastClicked.x = Mathf.Round((hit.point.x));
+                        lastClicked.y = Mathf.Round((hit.point.y));
+                        return;
+                    }
+                }
 
-                Debug.Log(mouseOver);
+                if (boardTileInstanceTypes[currMouseX, currMouseY] == "melee1")
+                {
+                    selectedUnit = boardTileInstances[currMouseX, currMouseY];
+                    lastClicked.x = Mathf.Round((hit.point.x));
+                    lastClicked.y = Mathf.Round((hit.point.y));
+                    return;
+                }
             }
         }
     }
---
+++ gameplay/Assets/Scripts/unit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unit : MonoBehaviour
+{
+    public int health;
+
+    private void Start()
+    {
+        health = 100;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
