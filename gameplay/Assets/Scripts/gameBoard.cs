b6fe962ed241aac267ef60abffdbf3bb3857ad63
Benjamin Loisch <ben.loisch@gmail.com>
2019-02-08 19:20:34 -0500

    Terrain Creation
    Added in a function that creates terrain from pre-defined input array

--- gameplay/Assets/Scripts/gameBoard.cs
+++ gameplay/Assets/Scripts/gameBoard.cs
@@ -6,12 +6,15 @@ public class gameBoard : MonoBehaviour
 {
     public static gameBoard Instance { set; get; }
 
-    //public terrainTiles[,] terrain = new terrainTiles[10, 10];
-    public string[,] boardTileInstanceTypes = new string[10, 10];
-    public GameObject[,] boardTileInstances = new GameObject[10, 10];
+    public string[,] terrainTileInstanceTypes = new string[10, 10];
+    public string[,] unitTileInstanceTypes = new string[10, 10];
+    public GameObject[,] unitTileInstances = new GameObject[10, 10];
     public Vector3 pz = new Vector3(-5.0f, 0.0f, 0.0f);
 
-    public GameObject terrain1Prefab;
+    public GameObject base1;
+    public GameObject terrain1;
+    public GameObject terrain2;
+    public GameObject terrain3;
     public GameObject melee1;
 
     public GameObject selectedUnit;
@@ -29,16 +32,27 @@ public class gameBoard : MonoBehaviour
 
         for (int i = 0; i < 10; i++)
             for (int j = 0; j < 10; j++)
-                boardTileInstances[i, j] = null;
+                unitTileInstances[i, j] = null;
 
         for (int i = 0; i < 10; i++)
             for (int j = 0; j < 10; j++)
-                boardTileInstanceTypes[i, j] = "empty";
+                unitTileInstanceTypes[i, j] = "empty";
 
         test = Instantiate(melee1) as GameObject;
-        test.transform.position = new Vector3(0, 0, -1);
-        boardTileInstances[0, 0] = test;
-        boardTileInstanceTypes[0, 0] = "melee1";
+        test.transform.position = new Vector3(0, 0, -2);
+        unitTileInstances[0, 0] = test;
+        unitTileInstanceTypes[0, 0] = "melee1";
+
+        string[,] terrainTileInstanceTypesTest = new string[10, 10];
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)
+                terrainTileInstanceTypesTest[i, j] = "empty";
+
+        terrainTileInstanceTypesTest[1, 1] = "terrain1";
+        terrainTileInstanceTypesTest[2, 2] = "terrain2";
+        terrainTileInstanceTypesTest[3, 3] = "terrain3";
+        terrainTileInstanceTypesTest[4, 4] = "base1";
+        initializeTerrain(terrainTileInstanceTypesTest);
 
     }
 
@@ -54,17 +68,17 @@ public class gameBoard : MonoBehaviour
 
                 Debug.Log(lastClicked);
 
-                if (boardTileInstanceTypes[currMouseX, currMouseY] == "empty")
+                if (unitTileInstanceTypes[currMouseX, currMouseY] == "empty")
                 {
                     if (selectedUnit == null)
                         return;
                     else
                     {
-                        selectedUnit.transform.position = new Vector3(currMouseX, currMouseY, -1);
-                        boardTileInstanceTypes[(int)lastClicked.x, (int)lastClicked.y] = "empty";
-                        boardTileInstances[(int)lastClicked.x, (int)lastClicked.y] = null;
-                        boardTileInstanceTypes[currMouseX, currMouseY] = "melee1";
-                        boardTileInstances[currMouseX, currMouseY] = selectedUnit;
+                        selectedUnit.transform.position = new Vector3(currMouseX, currMouseY, -2);
+                        unitTileInstanceTypes[(int)lastClicked.x, (int)lastClicked.y] = "empty";
+                        unitTileInstances[(int)lastClicked.x, (int)lastClicked.y] = null;
+                        unitTileInstanceTypes[currMouseX, currMouseY] = "melee1";
+                        unitTileInstances[currMouseX, currMouseY] = selectedUnit;
                         selectedUnit = null;
                         lastClicked.x = Mathf.Round((hit.point.x));
                         lastClicked.y = Mathf.Round((hit.point.y));
@@ -72,13 +86,42 @@ public class gameBoard : MonoBehaviour
                     }
                 }
 
-                if (boardTileInstanceTypes[currMouseX, currMouseY] == "melee1")
+                if (unitTileInstanceTypes[currMouseX, currMouseY] == "melee1")
                 {
-                    selectedUnit = boardTileInstances[currMouseX, currMouseY];
+                    selectedUnit = unitTileInstances[currMouseX, currMouseY];
                     lastClicked.x = Mathf.Round((hit.point.x));
                     lastClicked.y = Mathf.Round((hit.point.y));
                     return;
                 }
+            }
+        }
+    }
+
+    private void initializeTerrain(string[,] terrainInput)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                this.terrainTileInstanceTypes[i, j] = terrainInput[i, j];
+
+                if (terrainInput[i,j] == "terrain1")
+                {
+                    GameObject terrainTile = Instantiate(terrain1) as GameObject;
+                    terrainTile.transform.position = new Vector3(i, j, -1);
+                } else if (terrainInput[i, j] == "terrain2") {
+                    GameObject terrainTile = Instantiate(terrain2) as GameObject;
+                    terrainTile.transform.position = new Vector3(i, j, -1);
+                }
+                else if (terrainInput[i, j] == "terrain3") {
+                    GameObject terrainTile = Instantiate(terrain3) as GameObject;
+                    terrainTile.transform.position = new Vector3(i, j, -1);
+                }
+                else if (terrainInput[i, j] == "base1") {
+                    GameObject terrainTile = Instantiate(base1) as GameObject;
+                    terrainTile.transform.position = new Vector3(i, j, -1);
+                } 
+
             }
         }
     }
