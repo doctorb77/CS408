57f5dc8ad6d20d6d47c9bd18274a5dd9f1a5e67d
Benjamin Loisch <ben.loisch@gmail.com>
2019-02-13 18:42:11 -0500

    Underlying string board
    Begin changing board for strings

--- gameplay/Assets/Scripts/unit.cs
+++ gameplay/Assets/Scripts/unit.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class unit : MonoBehaviour
 {
     public int health;
+    public bool isPlayerOneUnit;
+    public float defense;
+    public string typeOfUnit;
 
     private void Start()
     {
         health = 100;
+        defense = 0.0;
     }
 
     private void Update()
